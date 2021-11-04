5a0b1d8b2af7e59b57e147453d8c352e37ef47a4
斎藤正大 <saima.ptw88@gmail.com>
2021-11-04 23:54:49 +0900

    add : QiitaArticle model spec

---
+++ spec/models/qiita_article_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe QiitaArticle, type: :model do
+  describe "field check" do
+    it { is_expected.to have_field(:title).of_type(String) }
+    it { is_expected.to have_field(:lgtm_count).of_type(Integer).with_default_value_of(0) }
+    it { is_expected.to have_field(:created_at).of_type(Time) }
+  end
+
+  describe "validation check" do
+    it { is_expected.to validate_presence_of(:title) }
+
+    it {
+      expect(subject).to validate_numericality_of(:lgtm_count).
+                           greater_than_or_equal_to(0).to_allow(only_integer: true, nil: false)
+    }
+
+    it { is_expected.to validate_presence_of(:created_at) }
+  end
+
+  describe "association check" do
+    context "reccomended_book" do
+      it { is_expected.to belong_to(:reccomended_book).as_inverse_of(:qiita_articles) }
+    end
+  end
+end
