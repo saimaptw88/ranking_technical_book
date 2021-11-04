4c8d0a93769ccb2f449728037ea59a88769d6dde
斎藤正大 <saima.ptw88@gmail.com>
2021-11-04 23:55:43 +0900

    add : ReccomendedBook model spec

---
+++ spec/models/reccomended_book_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe ReccomendedBook, type: :model do
+  describe "field check" do
+    it { is_expected.to have_field(:article_count).of_type(Integer).with_default_value_of(1) }
+  end
+
+  describe "validation check" do
+    context "article_count < 1" do
+      let(:reccomended_book) { build(:reccomended_book, :with_qiita_article, :with_qiita_tag, article_count: 0) }
+
+      it "error create document" do
+        expect(reccomended_book).to be_invalid
+      end
+    end
+
+    context "no qiita_article" do
+      let(:reccomended_book) { build(:reccomended_book, :with_qiita_tag, article_count: 0) }
+
+      it "error create document" do
+        expect(reccomended_book).to be_invalid
+      end
+    end
+
+    context "no qiita_tag" do
+      let(:reccomended_book) { create(:reccomended_book, :with_qiita_article, article_count: 0) }
+
+      it "success create document" do
+        expect(reccomended_book).to be_valid
+      end
+    end
+  end
+
+  describe "association check" do
+    context "qiita_articles" do
+      it { is_expected.to have_many(:qiita_articles).with_dependent(:destroy) }
+    end
+
+    context "qiita_tags" do
+      it { is_expected.to have_many(:qiita_tags).with_dependent(:destroy) }
+    end
+  end
+end
