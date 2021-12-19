c3462d848ddcac734af4c472066adbe7c98349d7
斎藤正大 <saima.ptw88@gmail.com>
2021-12-19 11:07:08 +0900

    add : home_controller

--- spec/requests/v1/home_spec.rb
+++ spec/requests/v1/home_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Homes", type: :request do
-  describe "GET /index" do
+  describe "GET /v1/home" do
+    subject { get(v1_home_index_path) }
+
+    before do
+      ReccomendedBook.destroy_all
+
+      b = create(:reccomended_book)
+      create(:amazon_affiliate, reccomended_book: b)
+    end
+
     it "returns http success" do
-      get "/v1/home/index"
+      subject
       expect(response).to have_http_status(:success)
     end
   end
