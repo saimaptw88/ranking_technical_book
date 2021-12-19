88f5dfde64c4ab8aa589436c3e3e86da0fb747dd
斎藤正大 <saima.ptw88@gmail.com>
2021-12-19 22:22:01 +0900

    add : limit article count

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -2,7 +2,7 @@ class V1::HomeController < ApplicationController
   def index
     # Redis から total_ranking 上位から順に記事を取得する
     ids_and_points = ReccomendedBook.pluck(:id, :total_ranking).sort_by {|_k, v| v }.reverse
-    books = ids_and_points.map {|k, _v| ReccomendedBook.find(k) }
+    books = ids_and_points.first(5).map {|k, _v| ReccomendedBook.find(k) }
 
     render json: books, each_serializer: V1::HomeSerializer
   end
