5e99cddddc31704cc2bf8671a04639246be419ff
斎藤正大 <saima.ptw88@gmail.com>
2021-12-26 09:25:38 +0900

    add : comtroller

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -1,8 +1,15 @@
 class V1::HomeController < ApplicationController
   def index
     # Redis から total_ranking 上位から順に記事を取得する
-    ids_and_points = ReccomendedBook.pluck(:id, :total_ranking).sort_by {|_k, v| v }.reverse
-    books = ids_and_points.first(5).map {|k, _v| ReccomendedBook.find(k) }
+    # ids_and_points = ReccomendedBook.pluck(:id, :total_ranking).sort_by {|_k, v| v }.reverse
+    # books = ids_and_points.first(5).map {|k, _v| ReccomendedBook.find(k) }
+
+    i = 0
+    books = []
+    ReccomendedBook.each do |book|
+      books << book if i < 5
+      i += 1
+    end
 
     render json: books, each_serializer: V1::HomeSerializer
   end
