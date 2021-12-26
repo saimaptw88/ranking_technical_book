b45fe6e5269cf050e33eca022d0a92d3897580e6
斎藤正大 <saima.ptw88@gmail.com>
2021-12-26 14:43:21 +0900

    retouch : handle to rspec

--- app/controllers/v1/ranking/title_controller.rb
+++ app/controllers/v1/ranking/title_controller.rb
@@ -1,7 +1,6 @@
 class V1::Ranking::TitleController < ApplicationController
   def index
     titles = []
-
     5.times do |i|
       id = Redis.current.get("total_ranking_#{i + 1}").to_s
       titles << ReccomendedBook.find(id).title
