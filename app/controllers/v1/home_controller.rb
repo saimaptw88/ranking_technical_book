d0bc84576703d63dbe063e56baa7dc10baa3c6ba
斎藤正大 <saima.ptw88@gmail.com>
2021-12-30 07:46:04 +0900

    retouch : delete comment outs

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -1,17 +1,7 @@
 class V1::HomeController < ApplicationController
   def index
     # Redis から total_ranking 上位から順に記事を取得する
-    # ids_and_points = ReccomendedBook.pluck(:id, :total_ranking).sort_by {|_k, v| v }.reverse
-    # books = ids_and_points.first(5).map {|k, _v| ReccomendedBook.find(k) }
-
-    # i = 0
     books = []
-
-    # ReccomendedBook.each do |book|
-    #   books << book if i < 5
-    #   i += 1
-    # end
-    # binding.pry
 
     5.times do |i|
       id = Redis.current.get("total_ranking_#{i + 1}")
