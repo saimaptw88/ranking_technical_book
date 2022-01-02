8a450bc14c349a0359c4f025f9068f66eb6be65b
斎藤正大 <saima.ptw88@gmail.com>
2022-01-02 17:50:12 +0900

    add : elements to serializer

--- app/serializers/v1/home_serializer.rb
+++ app/serializers/v1/home_serializer.rb
@@ -1,5 +1,6 @@
 class V1::HomeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :publication_data, :author, :explanation, :thumbnail_url, :total_point
+  attributes :id, :title, :publication_data, :author, :explanation, :thumbnail_url, :total_point, :yearly_point, :monthly_point, :total_ranking,
+             :yearly_ranking, :monthly_ranking
 
   def publication_data
     object.amazon_affiliate.publication_data
