648a9b85ec292cce9bc2f02219f06050e1995993
斎藤正大 <saima.ptw88@gmail.com>
2022-03-28 20:06:30 +0900

    add : tasks to clone

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,32 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# 参考記事
+# https://qiita.com/Esfahan/items/e7a924f7078faf3294f2
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+require "active_support/core_ext/time"
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-# Learn more: http://github.com/javan/whenever
+
+rails_env = ENV['RAILS_ENV'] || :development
+set :environment, rails_env
+
+
+# 時刻の文字列を日本時間で解釈して、UTCに変換
+def jst(time)
+  Time.zone = "Asia/Tokyo"
+  Time.zone.parse(time).localtime("+00:00")
+end
+
+
+every 1.day, at: jst("1:00 am") do
+  set :output, { error: "log/qiita_api_error.log", standard: "log/qiita_api_success.log" }
+  runner "QiitaApi.execute"
+end
+
+every 5.day, at: jst("5:00 am") do
+  set :output, { error: "log/calculate_ranking_error.log", standard: "log/calculate_ranking_success.log" }
+  runner "CalculateRanking.execute"
+end
+
+every 15.day, at: jst("2:00 pm") do
+  set :output, { error: "log/technical_book_search_api_error.log", standard: "log/technical_book_search_api_success.log" }
+  runner "TechnicalBooksSearchApi.execute"
+end
